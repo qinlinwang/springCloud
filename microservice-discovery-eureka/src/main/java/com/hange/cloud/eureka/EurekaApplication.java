6216717c91a505acfd6ddf0c82eafb511e107038
hange <hange@hange.com>
2021-01-04 20:25:50 +0800

    add

---
+++ microservice-discovery-eureka/src/main/java/com/hange/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.hange.cloud.eureka;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/**
+* 使用Eureka做服务发现。
+* @author wanghan
+*/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        //System.out.println("dddd");
+        new SpringApplicationBuilder(EurekaApplication.class).web(WebApplicationType.SERVLET).run(args);
+    }
+}
