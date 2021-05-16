66c1042ea113fdcfdc38f7054e45f573af00847a
hange <atnwang@qq.com>
2021-05-16 19:08:37 +0800

    添加了对hystrix的dashboard的支持

--- microservice-b-activity/microservice-b-activity-server/src/main/java/com/hange/cloud/activity/ActivityApplication.java
+++ microservice-b-activity/microservice-b-activity-server/src/main/java/com/hange/cloud/activity/ActivityApplication.java
@@ -3,7 +3,9 @@ package com.hange.cloud.activity;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -11,9 +13,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * 
  * @author wanghan
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.hange.cloud.activity","com.hange.cloud.user.feign"}) //这里添加对com.hange.cloud.activity的扫描
 @EnableDiscoveryClient // 服务注册到Eureka
 @EnableFeignClients(basePackages = {"com.hange.cloud.user.feign"}) //Scans for interfaces that declare they are feign clients
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class ActivityApplication {
     public static void main(String[] args) {
         //System.out.println("dddd");
